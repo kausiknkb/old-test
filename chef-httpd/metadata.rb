96240f4094366738a13d35549e248123becfeee8
kaushik <kaushik883@hotmail.com>
2014-07-27 00:14:36 +0530

    delete

--- chef-httpd/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             "httpd"
-maintainer       "Sascha Bates"
-description      "Installs/Configures httpd"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
-
-depends 'logrotate'
